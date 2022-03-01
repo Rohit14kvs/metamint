b72a40755283342289f6357e3bc48a044c9ca3ba
Rohit Singh <rohit14kvs@gmail.com>
2022-03-01 18:37:34 +0530

    Added routes for post comments

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import { Comment } from "../entities/Comment";
 import { Post } from "../entities/Post";
 import { Sub } from "../entities/Sub";
 import auth from "../middleware/auth";
@@ -45,7 +46,7 @@ const getPost = async (req: Request, res: Response) => {
     try {
         const post = await Post.findOneOrFail(
             { identifier, slug },
-            { relations: ['sub'] }
+            { relations: ['sub', 'comments'] }
         );
 
         return res.json(post);
@@ -55,10 +56,33 @@ const getPost = async (req: Request, res: Response) => {
     }
 }
 
+const commentOnPost = async (req: Request, res: Response) => {
+    const { identifier, slug } = req.params;
+    const body = req.body.body;
+
+    try {
+        const post = await Post.findOneOrFail({ identifier, slug })
+
+        const comment = new Comment({
+            body,
+            user: res.locals.user,
+            post
+        });
+
+        await comment.save();
+
+        return res.json(comment);
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json({ error: 'Post not found' })
+    }
+}
+
 const router = Router();
 
 router.post('/', auth, createPost);
 router.get('/', getPosts);
 router.get('/:identifier/:slug', getPost);
+router.post('/:identifier/:slug/comments', auth, commentOnPost);
 
 export default router;
