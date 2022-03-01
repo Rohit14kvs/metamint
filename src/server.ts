5cbd36646e0e438ceb45525f53c0c824a89fb425
Rohit Singh <rohit14kvs@gmail.com>
2022-03-01 15:33:09 +0530

    Created Post and Subs

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,13 @@ import { createConnection } from "typeorm";
 import express from "express";
 import morgan from "morgan";
 import authRoutes from "./routes/auth";
+import postRoutes from "./routes/posts";
+import subRoutes from './routes/subs';
 import trim from "./middleware/trim";
 import cookieParser from "cookie-parser";
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(morgan('dev'));
@@ -14,10 +17,12 @@ app.use(trim);
 app.use(cookieParser());
 
 app.get('/', (_, res) => res.send('Hello'));
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/subs', subRoutes);
 
-app.listen(5001, async () => {
-    console.log('Server running at http://localhost:5000');
+app.listen(PORT, async () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 
     try {
         await createConnection();
